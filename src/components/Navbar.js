05a2b09f046793fae69c08b65e1afa53926dddef
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2023-08-26 23:30:42 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,32 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineBell } from "react-icons/ai";
+import { BiVideoPlus } from "react-icons/bi";
+import { FaUserAlt } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
 export const Navbar = () => {
   return (
     <>
-      <h1>Navbar</h1>
+      <div className="flex">
+        <div>
+          <GiHamburgerMenu />
+          <img
+            className="w-12 h-12"
+            src="https://logowik.com/content/uploads/images/899_youtube_2017logo.jpg"
+            alt="youtube logo"
+          />
+        </div>
+        <div>
+          <input type="text"></input>
+          <button>
+            <AiOutlineSearch />
+          </button>
+        </div>
+        <div>
+          <BiVideoPlus />
+          <AiOutlineBell />
+          <FaUserAlt />
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/constants/Constants.js
@@ -0,0 +1,2 @@
+const URL =
+  "https://youtube.googleapis.com/youtube/v3/videos?chart=mostPopular&regionCode=IN&key=[YOUR_API_KEY]";
